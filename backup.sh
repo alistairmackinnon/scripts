2399f312195f3034fd81ca72f9cdf0cbb7d26e5b
Ali <alistair.mackinnon@outlook.com>
2020-06-15 17:26:26 +0100

    adding variables for portability

--- backup.sh
+++ backup.sh
@@ -2,11 +2,12 @@
 
 date=$(date +'%Y%m%d')
 backup_mount="/Volumes/BackUp"
+device_name="ali_mini"
 
 if mount | grep "on $backup_mount" > /dev/null; then
     for dir in Downloads Documents Photos Desktop dev 
     do
-        rsync -rtb --exclude='venv/' --backup-dir=Archive/$date --delete /Users/ali/$dir /Volumes/BackUp/ali_mini
+        rsync -rtb --exclude='venv/' --backup-dir=Archive/$date --delete /Users/ali/$dir $backup_mount/$device_name
         echo "$date rsync backup complete for $dir"
     done
 else
