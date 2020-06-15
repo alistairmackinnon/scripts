354e0a9aeefd9f4222c6aacdf1983184f5d33e8b
Ali <alistair.mackinnon@outlook.com>
2020-06-15 17:39:52 +0100

    adding comments

--- backup.sh
+++ backup.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
+# Bash script to check if a backup volume is mounted & use rsync to back up if it is present...
 
+
+# Set the date for the archive folder
 date=$(date +'%Y%m%d')
+# Mount Point for the backup drive
 backup_mount="/Volumes/BackUp"
+# Which device is it running on? Allows multiple devices
 device_name="ali_mini"
+# Define Directories to backup
+dirs="Downloads Documents Photos Desktop dev"
 
+# Check if backup volume is mounted
 if mount | grep "on $backup_mount" > /dev/null; then
-    for dir in Downloads Documents Photos Desktop dev 
+    # Backupo each folder independently to allow us to be selective over what gets backed up
+    for dir in $dirs 
     do
         rsync -rtb --exclude='venv/' --backup-dir=Archive/$date --delete /Users/ali/$dir $backup_mount/$device_name
         echo "$date rsync backup complete for $dir"
